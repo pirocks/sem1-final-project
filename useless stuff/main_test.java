1b4e3bdfdeafb44d9835a679a5c2ac74bf63aae6
Francis Nixon <13nixonf@gmail.com>
2015-12-14 00:30:46 +0000

    some of thos files

---
+++ useless stuff/main_test.java
@@ -0,0 +1,8 @@
+public class main_test
+{
+    public static void main(String[] args)
+    {
+        board global_board = utils.defualt_board;
+        command_line_ui.ask_for_piece_white(global_board,true,false);
+    }
+}
