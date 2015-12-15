643fa01bf7e4cdad1a669506d8c896b48dd3ecda
Francis Nixon <13nixonf@gmail.com>
2015-12-15 00:50:58 +0000

    an untracked file thatt got forgotten

---
+++ old_files/prune.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class prune
+{
+    public static move[] prune_internal(eval_move[] in,int len, prune_data data)
+    {
+        return to_move(prune_from_array(in,len),10);
+    }
+    public static eval_move[] prune_from_array(eval_move[] moves, int len)
+    {
+        //eval_move top5 = new eval_move[5];
+        //eval_move bottom5 = new eval_move[5];
+        //init bottom/top arrays
+        eval_move[] out = new eval_move[10];
+        Arrays.sort(moves);
+        for(int i = 0; i < 5;i++)
+        {
+            out[i] = moves[i];
+        }
+        int moves_i;
+        for(int i = 5; i < 10; i++)
+        {
+            moves_i = len - (i - 5);
+            out[i] = moves[moves_i];
+        }
+        return out;
+    }
+    public static move[] to_move(eval_move[] in, int len)
+    {
+        move[] out = new move[len];
+        for(int i =0; i < len;i++)
+            out[i] = new move(in[i]);
+        return out;
+    }
+    
+}
