a57699dbde166f84de86fa24039573db54d2ac66
Francis Nixon <13nixonf@gmail.com>
2015-12-15 01:05:23 +0000

    switchgin

--- Curses_command_line_ui.java
+++ Curses_command_line_ui.java
@@ -16,7 +16,7 @@ public class Curses_command_line_ui
             //experimental
             scored_board temp = new scored_board(global_board);
             positon_eval_refactor.dot_writers.init();
-            move current_move = new move(positon_eval_refactor.eval_top(temp,true,3,null,positon_eval_refactor.dot_writers.add_node()));
+            move current_move = new move(positon_eval_refactor.eval_top(temp,true,4,null,positon_eval_refactor.dot_writers.add_node()));
             //do ai moves
             positon_eval_refactor.dot_writers.close();
             global_board.apply_move(current_move);
@@ -78,7 +78,7 @@ public class Curses_command_line_ui
             //experimental
             scored_board temp = new scored_board(global_board);
             positon_eval_refactor.dot_writers.init();
-            move current_move = new move(positon_eval_refactor.eval_top(temp,false,3,null,positon_eval_refactor.dot_writers.add_node()));
+            move current_move = new move(positon_eval_refactor.eval_top(temp,false,4,null,positon_eval_refactor.dot_writers.add_node()));
             //do ai moves
             positon_eval_refactor.dot_writers.close();
             global_board.apply_move(current_move);
